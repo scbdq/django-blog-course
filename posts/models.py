a963bb8f446d260bba874798faa7345b83ce15bd
scbdq <93515882+scbdq@users.noreply.github.com>
2025-10-02 16:59:09 +0700

    feat: add author field to Post

--- posts/models.py
+++ posts/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     """Basic blog post entity."""
 
     title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(default=timezone.now)
 
