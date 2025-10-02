45fec859355ad1465b6bebfdd99164aea8d6b57b
scbdq <93515882+scbdq@users.noreply.github.com>
2025-10-02 17:25:49 +0700

    feat: add post detail view with tests

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 
 from .models import Post
 
@@ -21,3 +22,14 @@ def post_list_view(request):
         html.append(f"<li><strong>{post.title}</strong>: {snippet}...</li>")
     html.append('</ul>')
     return HttpResponse(''.join(html))
+
+
+def post_detail_view(request, post_id):
+    """Render details for a single post by ID."""
+    post = get_object_or_404(Post, id=post_id)
+    body = (
+        f'<h1>{post.title}</h1>'
+        f'<p><em>{post.author}</em></p>'
+        f'<div>{post.content}</div>'
+    )
+    return HttpResponse(body)
