6b9c9cf75fee3ce6ccf889ddd73c23e2615e84cd
scbdq <93515882+scbdq@users.noreply.github.com>
2025-10-02 16:45:07 +0700

    feat: add about page

--- posts/views.py
+++ posts/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 def home_view(request):
     """Render the blog homepage with a welcome message."""
     return HttpResponse("<h1>Добро пожаловать в мой блог!</h1>")
+
+
+def about_view(request):
+    """Render the about page with basic author info."""
+    return HttpResponse("<h1>Об авторе</h1><p>Добро пожаловать на страницу об авторе.</p>")
