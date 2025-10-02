09665188e74dced6b2d50c286906fcc4a6bb0039
scbdq <93515882+scbdq@users.noreply.github.com>
2025-10-02 16:46:37 +0700

    feat: add Post model with tests

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    """Basic blog post entity."""
+
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self) -> str:
+        return self.title
