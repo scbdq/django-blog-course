604c25c252bfc09567cf6853bd8ab244a458f643
scbdq <93515882+scbdq@users.noreply.github.com>
2025-10-02 17:24:09 +0700

    feat: add post list view with tests

--- posts/urls.py
+++ posts/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('about/', views.about_view, name='about'),
+    path('posts/', views.post_list_view, name='post_list'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 
+from .models import Post
 
 def home_view(request):
     """Render the blog homepage with a welcome message."""
@@ -9,3 +10,14 @@ def home_view(request):
 def about_view(request):
     """Render the about page with basic author info."""
     return HttpResponse("<h1>Об авторе</h1><p>Добро пожаловать на страницу об авторе.</p>")
+
+
+def post_list_view(request):
+    """Render a simple list of posts."""
+    posts = Post.objects.all()
+    html = ["<h1>Все посты</h1><ul>"]
+    for post in posts:
+        snippet = post.content[:50]
+        html.append(f"<li><strong>{post.title}</strong>: {snippet}...</li>")
+    html.append('</ul>')
+    return HttpResponse(''.join(html))
